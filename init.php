db47d89b5864f2fb271ebe5a846f2128a5480712
Сергей Ардинцев <ardicev@diorit.ru>
2026-09-24 15:58:40 +0300

    get it fix

--- init.php
+++ init.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect("localhost", "newuser", "", "yeticave");
-mysqli_set_charset($con, "utf8");
+$con = mysqli_connect('MySQL-8.0', 'root', '', 'yeticave', 3306);
+mysqli_set_charset($con, 'utf8mb4');
--- templates/main-lot.php
+++ templates/main-lot.php
@@ -1,12 +1,12 @@
 <section class="lot-item container">
-  <h2><?= lot["title"]; ?></h2>
+  <h2><?= $lot["title"]; ?></h2>
   <div class="lot-item__content">
     <div class="lot-item__left">
       <div class="lot-item__image">
-        <img src="<?= lot["img"]; ?>" width="730" height="548" alt="<?= lot["title"]; ?>">
+        <img src="<?= $lot["img"]; ?>" width="730" height="548" alt="<?= $lot["title"]; ?>">
       </div>
-      <p class="lot-item__category">Категория: <span><?= lot["name_category"]; ?></span></p>
-      <p class="lot-item__description"><?= lot["description"]; ?></p>
+      <p class="lot-item__category">Категория: <span><?= $lot["name_category"]; ?></span></p>
+      <p class="lot-item__description"><?= $lot["lot_description"]; ?></p>
     </div>
     <div class="lot-item__right">
       <div class="lot-item__state">
@@ -17,7 +17,7 @@
         <div class="lot-item__cost-state">
           <div class="lot-item__rate">
             <span class="lot-item__amount">Текущая цена</span>
-            <span class="lot-item__cost"><?= format_num($lot["start_price"]); ?></span>
+            <span class="lot-item__cost"><?= format_price($lot["start_price"]); ?></span>
           </div>
           <div class="lot-item__min-cost">
             Мин. ставка <span>12 000 р</span>
